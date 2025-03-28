2a2049354d902319919ced5f350c828f747d8229
jasleen <jasleenkaurkalsi03@gmail.com>
2025-03-28 14:03:46 -0500

    Add NPM scripts for development, build, start, and testing

---
+++ dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+// Middlewares
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+// Routes
+app.get('/', (req, res) => {
+    res.send('School API is running');
+});
+exports.default = app;
